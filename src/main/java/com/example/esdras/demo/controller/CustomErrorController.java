f155176afdfc4bbab9dec8e744c98902152553dc
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-08-26 11:21:14 -0300

    feat:adding validation handler

---
+++ src/main/java/com/example/esdras/demo/controller/CustomErrorController.java
@@ -0,0 +1,54 @@
+package com.example.esdras.demo.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+//SERVE COMO HANDLER PRA TODOS OS CONTROLLERS PRA MOSTRAR OS ERROS, TNATO DE VALIDATOR QUANTO DE JPA
+@ControllerAdvice
+public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException exception){
+
+        ResponseEntity.BodyBuilder responseEntity= ResponseEntity.badRequest();
+
+        if(exception.getCause().getCause() instanceof ConstraintViolationException){
+            ConstraintViolationException violationException = (ConstraintViolationException) exception.getCause().getCause();
+
+            List errorList = violationException.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                        Map<String, String > errorMap = new HashMap<>();
+                        errorMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                        return errorMap;
+                    }).collect(Collectors.toList());
+
+            return responseEntity.body(errorList);
+        }
+
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity handleBindErrors(MethodArgumentNotValidException exception){
+
+        List errorList = exception.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String > errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errorList);
+    }
+}
